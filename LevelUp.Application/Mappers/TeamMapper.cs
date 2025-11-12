43565bdff0b4a617fa3bcfbc42dabefa4053b02e
SunaUezuri <wesley.jane.santos@gmail.com>
2025-11-11 21:51:59 -0300

    feat: Criando um mapper para Team

---
+++ LevelUp.Application/Mappers/TeamMapper.cs
@@ -0,0 +1,25 @@
+﻿using LevelUp.Application.Dtos.Team;
+using LevelUp.Domain.Entities;
+
+namespace LevelUp.Application.Mappers
+{
+    public static class TeamMapper
+    {
+
+        public static TeamEntity ToEntity(this TeamCreateUpdateDto dto)
+        {
+            return new TeamEntity
+            {
+                TeamName = dto.TeamName
+            };
+        }
+
+        public static TeamResponseDto ToResponseDto(this TeamEntity team)
+        {
+            return new TeamResponseDto(
+                            team.Id,
+                            team.TeamName
+                        );
+        }
+    }
+}
